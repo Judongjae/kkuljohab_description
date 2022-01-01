264c1d2bc71bb83900cc36d6c40d40f6766200c5
Judongjae <wnehdwo0982@naver.com>
2022-01-01 21:52:24 +0900

    1일1커밋시작

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+//express 모듈을 쓰겠다.
+import dotenv from "dotenv";
+//dotenv모듈을 쓰겠다.
+import mongoose from "mongoose";
+//mongoose 모듈을 쓰겠다.
+import postRouter from "../src/routes/postRouter.js";
+
+const app = express();
+const port = 3000;
+
+app.use("api/v1/post", postRouter);
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log("Express server has started on port " + port);
+});
---
+++ src/models/Post.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+//mongoose 모듈을 쓰겠다.
+
+const postSchema = new mongoose.Schema(
+  // postSchema는 새로운 몽구스 스키마이다. 그 구성은 아래와 같다.
+  {
+    postId: {
+      type: String,
+      required: false, //
+      unique: false,
+    },
+    postTitle: {
+      type: String,
+      required: false,
+      unique: false,
+    },
+  },
+  { versionKey: false }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export { Post, Like };
