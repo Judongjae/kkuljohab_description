0bbffb18e329e9d8b735784d477b14af4a6c4bdc
Judongjae <wnehdwo0982@naver.com>
2022-01-21 18:21:14 +0900

    Post = mongoose.model 이건 객체라는 건가?

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,9 +1,10 @@
 import { Post } from "../models/Post.js";
-
+//저 위치의 post를 수입한다.
 const postDate = new Date();
 
 function dateFormat(date) {
   let month = date.getMonth() + 1;
+  //왜 여기엔 +1을 할까?
   let day = date.getDate();
   let hour = date.getHours();
   let minute = date.getMinutes();
@@ -27,6 +28,7 @@ function dateFormat(date) {
     second
   );
 }
+//이걸 해석해보자
 
 export const postcreate = async (req, res) => {
   const post = new Post();
@@ -50,7 +52,9 @@ export const postcreate = async (req, res) => {
 
 export const postfind = async (req, res) => {
   Post.findOne({ _id: req.params.postid }, (err, post) => {
-    if (err) return res.status(500).send({ error: "못찾겠음" });
+    //하나를 찾고
+    if (err) return res.status(500).send({ error: "에러떴슴" });
+    //없는경우
     if (!post)
       return res
         .status(400)
@@ -61,6 +65,7 @@ export const postfind = async (req, res) => {
 
 export const postupdate = async (req, res) => {
   Post.findOneAndUpdate({ _id: req.params.postid }, (err, post) => {
+    //findone 말고 findoneandupdate를 쓸 순 없을까?
     if (err) return res.status(500).send({ error: "수정안됨" });
     if (!post)
       return res
@@ -69,15 +74,7 @@ export const postupdate = async (req, res) => {
     res.status(200).send(post);
   });
 };
-
-// export const postupdate = async (req, res) => {
-//   Post.findOne({ _id: req.params.postid }, (err, post) => {
-//     if(err) return res.status(500).send({ error: "Datebase Failure!"})
-//   });
-//   try {
-//     const { postTitle, postContent } = req.body;
-//   } catch {}
-// };
-
-//dddd
-//dddd
+//제목이 같은 글 다섯개가 있으면 그 이상 같은 제목의 게시글을
+//못만들게 하는 함수에서  게시글 하나 만들 때 마다
+//데이터베이스를 돌리는건 오버워킹?이다. 이걸
+//어찌해야 될까?
--- src/models/Post.js
+++ src/models/Post.js
@@ -6,29 +6,37 @@ const postSchema = new mongoose.Schema(
   {
     postId: {
       type: String,
-      required: false, //
-      unique: false,
+      //문자열타입
+      required: true,
+      // not null과 같은 기능 true면 값이 있어야만 함 null값이 들어갈 수 없음
+      unique: true,
+      //true면 고유한 값이 들어가야함
     },
     postTitle: {
       type: String,
-      required: false,
+      required: true,
       unique: false,
     },
     postContent: {
       type: String,
-      required: false,
+      required: true,
       unique: false,
     },
     currentDate: {
       type: String,
-      required: false,
+      required: true,
       unique: false,
     },
   },
   { versionKey: false }
+  //postman에서 보이는 걸리적 거리던 버젼키를 없앰
 );
 
 const Post = mongoose.model("Post", postSchema);
-
+//이게 뭔소리일까?
+//Post라는 객체를 만들겠다는 뜻?
+//이건 일종의 틀 인건가?
 export { Post };
 //post를 내보낸다.
+//Post 모델을 참고할 수 있게 한다.
+//위 객체를 사용할 수 있게한다?
